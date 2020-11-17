16398457023cde5ffaec94e75ec78b323bb3e280
Seaman51 <seamanmurmansk@gmail.com>
2020-11-17 21:45:02 +0300

    HW Lesson 7

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -13,3 +13,31 @@
 arr[m]
 from statistics import median
 """
+
+from random import random
+from statistics import median
+import heapq
+
+
+def median1(l):
+    nl = []
+    heapq.heapify(nl)
+    for i in range(len(l) // 2 + 1):
+        heapq.heappush(nl, l[i])
+    for i in range(len(l) // 2 + 1, len(l)):
+        if l[i] > nl[0]:
+            heapq.heapreplace(nl, l[i])
+    return nl[0]
+
+
+m = 2 * int(input('Построим массив длиной 2m + 1. Введите m ')) + 1
+
+lst = [int(random() * 100) for i in range(m)]
+
+print(lst)
+print(median1(lst))
+print(median(lst))  # для проверки
+
+"""
+встроенная функция быстрее
+"""
